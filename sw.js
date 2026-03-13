d964b3ee053f5d9db0240850261fefc55f5124fd
AWMCLICKFOOD <andersoncraveiro47@gmail.com>
2026-03-13 15:52:16 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'caixinha-wm-v1';
+
+// Aqui nós dizemos ao celular exatamente quais arquivos ele deve guardar para usar offline
+const arquivosParaGuardar = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './LOGOTIPO.jpg',
+  './Captura de tela 2026-02-13 132630.jpg',
+  './icone.jpg'
+];
+
+self.addEventListener('install', evento => {
+  evento.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(arquivosParaGuardar))
+  );
+});
+
+self.addEventListener('fetch', evento => {
+  evento.respondWith(
+    caches.match(evento.request)
+      .then(resposta => resposta || fetch(evento.request))
+  );
+});
