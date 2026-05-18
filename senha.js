c2e8d1f3e1ea5e3cfa8b4460a5d07b809313bb87
ANDERSONREZENDEE <andersoncraveiro47@gmail.com>
2026-05-18 14:18:18 -0300

    Delete senha.js

--- senha.js
+++
@@ -1,7 +0,0 @@
-// ==========================================
-// ARQUIVO DE SENHA - CAIXINHA WM / CONTROLADORIA
-// Suba este arquivo junto com index.html, manifest.json e sw.js.
-// A senha/hash não fica mais dentro do index.html.
-// ==========================================
-
-window.HASH_SENHA_MESTRA = "544f77a1f22a352c4160858b2d787cba9576be6d70b0ed53b74a8139ca861a61";
